a88027b7a2406e5a8de4c514bf783c48f203ae7b
Russo <anuo@gft.com>
2022-12-23 11:10:42 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake=[
+    {
+        "id": "1",
+        "photo": "https://algaworks-blog.s3.amazonaws.com/wp-content/uploads/Angular.png",
+        "title": "NOVA VERSÃO DO ANGULAR",
+        "description": "bla bla bla "
+    },
+    {
+        "id": "2",
+        "photo": "https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg",
+        "title": "saiu nova serie",
+        "description": "ha ha ha ha"
+    },
+]
